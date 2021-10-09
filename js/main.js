56bb898b9a32e2ba41c26962c794c7f1349f6db4
PumYulie <yulisakova@gmail.com>
2021-10-09 18:56:45 +0300

    пофиксила

--- js/main.js
+++ js/main.js
@@ -1,37 +1,5 @@
-/* function randomInteger(min, max) {
-  // источник learn.javascript.ru/task/random-int-min-max
-  if (min < 0 || max < 0) {
-    throw('впиши плз 2 разных целых положительных числа, 0 тоже подходит');
-  }
-
-  if(min >= max) {
-    throw('впиши плз 2 разных целых положительных числа, 0 тоже подходит');
-  }
-
-  const rand = min + Math.random() * (max + 1 - min);
-  return Math.floor(rand);
-}
-
-function randomIntegerWithDot(min, max, integersAfterDot) {
-  if (min < 0 || max < 0) {
-    throw('впиши плз 2 разных положительных числа, можно нецелых, 0 тоже подходит');
-  }
-
-  if(min >= max) {
-    throw('впиши плз 2 разных положительных числа, можно нецелых, 0 тоже подходит');
-  }
-
-  integersAfterDot = Math.round(integersAfterDot);
-  const num = min + Math.random() * (max - min);
-  return +num.toFixed(integersAfterDot);
-}
-
-randomInteger(1, 4);
-randomIntegerWithDot(1, 3, 3);
- */
-
-import {getRandomPositiveInteger} from './utils/get-random-positive-integer';
-import {getRandomPositiveFloat} from './utils/get-random-positive-float';
+import {getRandomPositiveInteger} from './utils/get-random-positive-integer.js';
+import {getRandomPositiveFloat} from './utils/get-random-positive-float.js';
 
 const TYPEOFESTATE = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
 const CHECKINS = ['12:00', '13:00', '14:00'];
@@ -47,7 +15,7 @@ const generateFatObject = () => {
 
   const getAvatarDigits = () => {
     const number = getRandomPositiveInteger(1, 10);
-    number < 10 ? (`0${number}`) : String(number);
+    return number < 10 ? `0${number}` : `${number}`;
   };
 
   const getArrayRandomElement = (array) => {
@@ -56,9 +24,6 @@ const generateFatObject = () => {
   };
 
   const createArrayFromRandomElements = (array, arrayLength) => {
-    if (arrayLength > array.length) {
-      throw('В массиве не так много элементов, укажи меньшее число вторым аргументом');
-    }
     const createdArray = [];
     for (let index = 0; index <= arrayLength; index++) {
       const element = getArrayRandomElement(array);
@@ -69,17 +34,20 @@ const generateFatObject = () => {
     return createdArray;
   };
 
+  const LATITUDE = getRandomPositiveFloat(35.65000, 35.70000, 5);
+  const LONGITUDE = getRandomPositiveFloat(139.70000, 139.80000, 5)
+
   return {
     author: {
       avatar: `img/avatars/user${getAvatarDigits()}.png`,
     },
     offer: {
       title: 'Вариант, где остановиться',
-      address: `${this.location.lat}, ${this.location.lng}`,
-      price: getRandomPositiveInteger(0, 1e12),
+      address: `${LATITUDE}, ${LONGITUDE}`,
+      price: getRandomPositiveInteger(0, 4000),
       type: getArrayRandomElement(TYPEOFESTATE),
-      rooms: getRandomPositiveInteger(1, 1e12),
-      guests: getRandomPositiveInteger(1, 1e12),
+      rooms: getRandomPositiveInteger(1, 30),
+      guests: getRandomPositiveInteger(1, 60),
       checkin: getArrayRandomElement(CHECKINS),
       checkout: getArrayRandomElement(CHECKOUTS),
       features: createArrayFromRandomElements(FEATURES, getRandomPositiveInteger(0, FEATURES.length)),
@@ -87,14 +55,10 @@ const generateFatObject = () => {
       photos: createArrayFromRandomElements(PHOTOS, getRandomPositiveInteger(0, PHOTOS.length)),
     },
     location: {
-      lat: getRandomPositiveFloat(35.65000, 35.70000, 5),
-      lng: getRandomPositiveFloat(139.70000, 139.80000, 5),
+      lat: LATITUDE,
+      lng: LONGITUDE,
     },
   };
 };
 
 const ArrayOfGeneratedObjects = Array.from({length: 10}, generateFatObject);
-
-ArrayOfGeneratedObjects();
-
-//console.log(ArrayOfGeneratedObjects);
