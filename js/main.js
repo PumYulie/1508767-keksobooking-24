312787eab04e0b5c93346305aa952e8c798c9771
PumYulie <yulisakova@gmail.com>
2021-11-11 23:55:43 +0300

    again

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ import {popupArticlesGenerated} from './generate-similar-elems.js';
 import {formAdTitle, formCoordinates, formPlaceType, formPriceInput, formTimein, formRoomNumber, saveFormButton} from './form-validation.js';
 import {makePageInactive, makePegaActive} from './page-activation.js';
 
-makePageInactive;
+makePageInactive();
 popupArticlesGenerated;
 formRoomNumber;
 saveFormButton;
