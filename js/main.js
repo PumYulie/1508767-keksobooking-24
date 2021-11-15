179bab0e1ace63646d810060c09a9ab96e130aa1
PumYulie <yulisakova@gmail.com>
2021-11-15 16:14:37 +0300

    3files changed

--- js/main.js
+++ js/main.js
@@ -1,15 +1,4 @@
-import {popupArticlesGenerated} from './generate-similar-elems.js';
-import {formAdTitle, formCoordinates, formPlaceType, formPriceInput, formTimein, formRoomNumber, saveFormButton} from './form-validation.js';
-import {makePageInactive, makePegaActive} from './page-activation.js';
-
-makePageInactive();
-popupArticlesGenerated;
-formRoomNumber;
-saveFormButton;
-formAdTitle;
-formPriceInput;
-formCoordinates;
-formPlaceType;
-formTimein;
-makePegaActive;
-
+import './map-form.js';
+import './user-form.js';
+import './filters.js';
+import './avatar.js';
